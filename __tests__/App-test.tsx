e636c8fadcd93d3c7ad3a05abd19dbabecd10d22
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2023-03-08 01:10:46 +0300

    feat: add redux

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,13 +1,10 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import {App} from '~/components/app';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+import {App} from '~/components/app';
+
+import 'react-native';
 
 it('renders correctly', () => {
   renderer.create(<App />);
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Provider} from 'react-redux';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {Routes, routes} from '~/routes';
+import {store} from '~/store';
+
+const {Screen, Navigator} = createNativeStackNavigator();
 
 export const App = () => (
-  <SafeAreaView>
-    <Text>Finance Mobile</Text>
-  </SafeAreaView>
+  <Provider store={store}>
+    <NavigationContainer>
+      <Navigator
+        initialRouteName={Routes.MAIN}
+        screenOptions={{headerShown: false}}>
+        <Screen {...routes.main} />
+      </Navigator>
+    </NavigationContainer>
+  </Provider>
 );
