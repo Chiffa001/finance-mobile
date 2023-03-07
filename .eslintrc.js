f0c07f7a7f2062f0c7a7e388ff82e35d8606ddbb
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2023-03-08 01:10:46 +0300

    feat: add redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,32 @@
 module.exports = {
   root: true,
+  plugins: ['simple-import-sort'],
   extends: '@react-native-community',
   rules: {
     'react/function-component-definition': [
       2,
       {namedComponents: 'arrow-function'},
+    ],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // `react` first, `next` second, then packages starting with a character
+          ['^react$', '^next', '^[a-z]'],
+          // Packages starting with `@`
+          ['^@'],
+          // Packages starting with `~`
+          ['^~'],
+          // Imports starting with `../`
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Imports starting with `./`
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports
+          ['^.+\\.s?css$'],
+          // Side effect imports
+          ['^\\u0000'],
+        ],
+      },
     ],
   },
 };
---
+++ src/screens/main-screen/main-screen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export const MainScreen = () => (
+  <SafeAreaView>
+    <Text>Finance Mobile</Text>
+  </SafeAreaView>
+);
