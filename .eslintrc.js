eea79875290ee3f9876187266bf89fb75af1bad4
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2023-03-07 12:43:13 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {namedComponents: 'arrow-function'},
+    ],
+  },
+};
