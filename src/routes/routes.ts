09d3de21d356ba4dcca07904522d6e287da45018
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2023-03-15 16:40:41 +0300

    feat: add bottom tabs

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,18 +1,24 @@
 import {AuthScreen} from '~/screens/auth-screen';
 import {MainScreen} from '~/screens/main-screen';
+import {MainStackScreen} from '~/screens/main-stack-screen/main-stack-screen';
+import {StatisticsScreen} from '~/screens/statistics-screen';
+import {Route, RouteNames, Routes} from '~/types/navigation';
 
-export enum Routes {
-  MAIN = 'MAIN_PAGE',
-  REGISTRATION = 'REGISTRATION_PAGE',
-}
-
-export const routes = {
-  main: {
+export const routes: Record<RouteNames, Route> = {
+  [RouteNames.MAIN]: {
     name: Routes.MAIN,
     component: MainScreen,
   },
-  registration: {
-    name: Routes.REGISTRATION,
+  [RouteNames.MAIN_STACK]: {
+    name: Routes.MAIN_STACK,
+    component: MainStackScreen,
+  },
+  [RouteNames.AUTHORIZATION]: {
+    name: Routes.AUTHORIZATION,
     component: AuthScreen,
+  },
+  [RouteNames.STATISTICS]: {
+    name: Routes.STATISTICS,
+    component: StatisticsScreen,
   },
 };
