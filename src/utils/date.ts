7d4884f6ddf0a1c03a580d18a071df47a4eb4d89
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2023-03-23 18:39:11 +0300

    feat: add ci

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,4 @@
-const options = {
+const staticOptions = {
   year: 'numeric',
   month: 'numeric',
   day: 'numeric',
@@ -7,10 +7,11 @@ const options = {
   hour12: false,
 } as const;
 
-export const getDateTime = (date: Date | string) => {
+export const getDateTime = (date: Date | string, timeZone?: string) => {
   if (!(date instanceof Date)) {
     date = new Date(date);
   }
+  const options = timeZone ? {...staticOptions, timeZone} : staticOptions;
   const [formattedDate, time] = new Intl.DateTimeFormat('ru', options)
     .format(date)
     .split(', ');
